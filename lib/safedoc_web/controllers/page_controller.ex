e587a41ce4907edd2fbad3e7e3c339232d966799
Nelson Junior <junior123nelson@gmail.com>
2019-11-20 12:00:23 -0300

    criando cards da home

--- lib/safedoc_web/controllers/page_controller.ex
+++ lib/safedoc_web/controllers/page_controller.ex
@@ -1,8 +1,11 @@
 defmodule SafedocWeb.PageController do
   use SafedocWeb, :controller
+  alias Safedoc.Account
+  alias Safedoc.Account.Customer
 
   def index(conn, _params) do
+    customers = Account.list_customers()
     user = Guardian.Plug.current_resource(conn)
-    render(conn, "index.html")
+    render(conn, "index.html", customers: customers)
   end
 end
